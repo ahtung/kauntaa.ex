b5b2187f0f792a4a6549a98c9a6b7dcf8fb06332
Dunya Kirkali <dunyakirkali@gmail.com>
2019-05-31 15:29:17 +0200

    Prepare for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :kauntaa, KauntaaWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "gentle-lowlands-15973", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
